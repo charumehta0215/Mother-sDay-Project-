fb4b358f82b9fdc0b2c5e2036aedec30613fd164
charumehta0215 <mehtacharu0215@gmail.com>
2024-08-15 23:56:00 +0530

    Complete

---
+++ app/src/main/java/com/example/mothersday/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.mothersday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModel;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void OpenActivity(View v){
+        Toast.makeText(this, "Welcome", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/mothersday/MainActivity2.java
@@ -0,0 +1,36 @@
+package com.example.mothersday;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void Press(View v){
+        Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MainActivity3.class);
+        startActivity(intent);
+    }
+    public void mainactivity4(View v){
+        Intent intent = new Intent (this,MainActivity4.class);
+        startActivity(intent);
+    }
+}
